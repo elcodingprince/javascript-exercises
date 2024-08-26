023233b95e7afc5a39e81f076851622b31abf729
elcodingprince <167471071+elcodingprince@users.noreply.github.com>
2024-08-26 07:40:44 -0700

    my first body of code split to array, reversed array, and then joined back to string on step at a time by storing each step in its n variable.
    i decided to join each funtion in one line of code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  // going to try to add all funciton on one line 
+    return string.split('').reverse().join('');
+  
+  
+    /*  let result = '';
+    let stringToArray = string.split('');
+    let reversedArray = stringToArray.reverse();
+    let arraytoString = reversedArray.join('');
+    result += arraytoString;
+    return result; */
 
 };
 
