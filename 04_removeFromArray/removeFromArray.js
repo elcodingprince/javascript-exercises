d5c3b506a6c289c2732cd50862b83398433e13a7
elcodingprince <167471071+elcodingprince@users.noreply.github.com>
2024-08-27 16:51:57 -0700

    completed test 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) {
+        // Convert valuesToRemove to a Set for efficient look-up
+        const setToRemove = new Set(valuesToRemove);
+    
+        // Filter the array, only keeping elements not in the setToRemove
+        return array.filter(item => !setToRemove.has(item));
+    
 };
 
 // Do not edit below this line
