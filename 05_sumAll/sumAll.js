489d543c537687b9a455e81e026a2fd80b570790
elcodingprince <167471071+elcodingprince@users.noreply.github.com>
2024-08-28 08:13:58 -0700

    added for loop that only loops until the smaller number equals the bigger number.
    the smaller number gets returned to finalSum value then the samller number gets incremnted. the incremented smaller number then gets added back to the finalSum value until the incremented smaller number uquals the larger number that was argumented

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,5 @@
 const sumAll = function(numOne, numTwo) {
-    if (numOne < 0 || numTwo < 0 || !isNaN(numOne) || !isNaN(numTwo)) return "ERROR";
+    if (numOne < 0 || numTwo < 0 || isNaN(numOne) || isNaN(numTwo)) return "ERROR";
     let finalSum = 0;
     let smallerNum;
     let largerNum; 
@@ -10,6 +10,10 @@ const sumAll = function(numOne, numTwo) {
         smallerNum = numTwo;
         largerNum = numOne;
     }
+    for (let i = smallerNum; i <= largerNum; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
