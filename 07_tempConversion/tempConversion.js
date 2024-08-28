5cb511c2419161a85018ac09558504aff6e0862b
elcodingprince <167471071+elcodingprince@users.noreply.github.com>
2024-08-28 15:16:51 -0700

    got rid of result varibale and stores the fahreneit to celsiuc conversion inside of new temo variable and changed it to const

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,8 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const newTemp = (fahrenheit - 32) * 5 / 9;
+  result += newTemp
+  const roundedNewTemp = Math.round(result * 10) / 10;
+  return roundedNewTemp;
 };
 
 const convertToFahrenheit = function() {
