fd313af43a3301107b71edb510b0a586c8b0ae68
elcodingprince <167471071+elcodingprince@users.noreply.github.com>
2024-08-28 08:45:47 -0700

    moved finalSum variable to be right before the for loop where its being used

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,6 @@
 const sumAll = function(numOne, numTwo) {
     if (numOne < 0 || numTwo < 0 || !Number.isInteger(numOne) || !Number.isInteger(numTwo)) return "ERROR";
 
-    let finalSum = 0;
     let smallerNum;
     let largerNum; 
     if (numOne < numTwo) {
@@ -11,6 +10,8 @@ const sumAll = function(numOne, numTwo) {
         smallerNum = numTwo;
         largerNum = numOne;
     }
+
+    let finalSum = 0;
     for (let i = smallerNum; i <= largerNum; i++) {
         finalSum += i;
     }
