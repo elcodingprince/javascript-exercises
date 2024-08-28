83f0fc0897280c68889b02ad960fa8fb3d706e11
elcodingprince <167471071+elcodingprince@users.noreply.github.com>
2024-08-28 08:36:36 -0700

    syntax error

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,5 @@
 const sumAll = function(numOne, numTwo) {
-    if (numOne < 0 || numTwo < 0 || !Number.isInteger(numOne) || Number.isInteger(numTwo)) return "ERROR";
+    if (numOne < 0 || numTwo < 0 || !Number.isInteger(numOne) || !Number.isInteger(numTwo)) return "ERROR";
 
     let finalSum = 0;
     let smallerNum;
