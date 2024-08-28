a522c09f8832231a5a3317b070ec14d793ea399e
elcodingprince <167471071+elcodingprince@users.noreply.github.com>
2024-08-28 07:24:14 -0700

    added a finalSum varibale with the init value of 0

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,6 @@
 const sumAll = function(numOne, numTwo) {
     if (numOne < 0 || numTwo < 0 || !isNaN(numOne) || !isNaN(numTwo)) return "ERROR";
+    let finalSum = 0;
 };
 
 // Do not edit below this line
