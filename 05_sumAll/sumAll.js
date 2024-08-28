b7ac642076186008930be34ee3c38e3ea1a35c9e
elcodingprince <167471071+elcodingprince@users.noreply.github.com>
2024-08-28 07:40:55 -0700

    added an if statement to define the two different varibales that i created
    the smaller number will always equal smallerNum and the bigger number will always equal largerNum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
 const sumAll = function(numOne, numTwo) {
     if (numOne < 0 || numTwo < 0 || !isNaN(numOne) || !isNaN(numTwo)) return "ERROR";
     let finalSum = 0;
+    let smallerNum;
+    let largerNum; 
+    if (numOne < numTwo) {
+        smallerNum = numOne;
+        largerNum = numTwo; 
+    } else {
+        smallerNum = numTwo;
+        largerNum = numOne;
+    }
 };
 
 // Do not edit below this line
